2df26dcad2417ba5236e1a86a8d2caa47ce1fd75
rocky <>
2020-12-16 16:44:06 +0800

    ascii

---
+++ _1_basic/_1string/_4_rune_char/rune_test.go
@@ -0,0 +1,13 @@
+package _4_rune_char
+
+import (
+	"fmt"
+	"testing"
+)
+
+// ascii
+// https://segmentfault.com/q/1010000000404709
+func TestChar(t *testing.T) {
+	fmt.Println(fmt.Sprintf("%d", 'a'))
+	fmt.Println(fmt.Sprintf("%c", 65))
+}
