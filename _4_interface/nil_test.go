02c100162128ca2e118007ff819caedf81274efa
rocky <>
2021-02-23 11:48:12 +0800

    nil interface

--- _4_interface/nil_test.go
+++ _4_interface/nil_test.go
@@ -13,7 +13,6 @@ type Person struct {
 
 // interface在golang内部其实是一个结构体，有_type,data两个指针组成
 // https://studygolang.com/articles/1749
-// https://studygolang.com/articles/5730
 //
 func TestNil(t *testing.T) {
 	var man *Person = nil
@@ -23,4 +22,14 @@ func TestNil(t *testing.T) {
 	fmt.Printf("ai == ei: %v\n", ai == ei)
 	fmt.Printf("ei == man: %v\n", ei == man)
 	fmt.Printf("ei == nil: %v\n", ei == nil)
+}
+
+// https://studygolang.com/articles/1908
+func TestNil1(t *testing.T) {
+	var val interface{} = (*interface{})(nil)
+	if val == nil {
+		fmt.Println("val is nil")
+	} else {
+		fmt.Println("val is not nil")
+	}
 }
